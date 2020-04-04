6276a360c2d53cc9b54668e230793289482b8e23
bnsdesenvolvimento2 <55952940+bnsdesenvolvimento2@users.noreply.github.com>
2020-04-04 14:47:16 -0300

    Add files via upload

---
+++ scope.js
@@ -0,0 +1,23 @@
+/*Programa scope.js
+Descrição: Este programa mostra a ordem de execução de variáveis a depender
+de ser escopo local ou global.
+Autor: Nelson S. dos Santos
+Data: 02/04/2020.
+*/
+
+
+// Definição de funções
+
+function checkVariableScope(){
+	var demoVar = "local scope";
+	document.write(demoVar);
+}
+
+
+// Declaração de variáveis 
+
+var demoVar = "global scope";
+
+
+
+// Saída de dados
