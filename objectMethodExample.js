041656bdfb822ecc1bc68a21573f719a44d1c7f4
bnsdesenvolvimento2 <55952940+bnsdesenvolvimento2@users.noreply.github.com>
2020-04-04 19:47:15 -0300

    Add files via upload

---
+++ objectMethodExample.js
@@ -0,0 +1,9 @@
+/* Programa objectMethodExample
+Descrição: Este programa ensina o uso de objetos em javascript,
+atentando para o fato de não ser necessário escrever classes para obter OOP.
+Autor: Nelson Seixas dos Santos
+Data: 04/04/2020.
+Versão: 0.0.1
+*/
+document.write("Welcome to JavaScript");
+
---
+++ objectsExample.js
@@ -0,0 +1,15 @@
+/* Programa objectsExample
+Descrição: Este programa ensina o uso de objetos em javascript,
+atentando para o fato de não ser necessário escrever classes para obter OOP.
+Autor: Nelson Seixas dos Santos
+Data: 04/04/2020.
+Versão: 0.0.1
+*/
+function titulo() {
+    objectName.objectProperty = propertyValue;
+    var str = document.title;
+    document.body.write(str)
+    
+}
+
+
